3f793481fd9d4b74cf90fe7bebc41364891a49c4
MrrDemaN <dmitry.kachan@trilogo.site>
2024-04-03 19:36:15 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
-window.addEventListener("DOMContentLoaded", () => {
-  console.log("Loaded Scripts");
+$(document).on("click", ".burger", function () {
+  var aside = $(".dashboard__aside");
+  if (!aside.hasClass("show")) {
+    aside.addClass("show");
+  } 
+});
+$(document).on("click", ".close-burger", function () {
+  var aside = $(".dashboard__aside");
+  if (aside.hasClass("show")) {
+    aside.removeClass("show");
+  } 
 });
